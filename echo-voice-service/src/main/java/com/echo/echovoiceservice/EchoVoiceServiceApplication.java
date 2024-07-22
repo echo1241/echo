f481c86a932d4d575e4c94847d3268d86675388b
hyun <hyun1202a@gmail.com>
2024-07-22 11:34:49 +0900

    git init

---
+++ echo-voice-service/src/main/java/com/echo/echovoiceservice/EchoVoiceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.echo.echovoiceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchoVoiceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EchoVoiceServiceApplication.class, args);
+	}
+
+}
---
+++ echo-voice-service/src/test/java/com/echo/echovoiceservice/EchoVoiceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.echo.echovoiceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EchoVoiceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
