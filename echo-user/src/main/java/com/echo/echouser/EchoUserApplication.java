ec72d241d9fb3de8a7ef3c8a12d6740c1439360d
hyun <hyun1202a@gmail.com>
2024-07-22 16:23:03 +0900

    echo-user module init

---
+++ echo-user/src/main/java/com/echo/echouser/EchoUserApplication.java
@@ -0,0 +1,13 @@
+package com.echo.echouser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchoUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EchoUserApplication.class, args);
+    }
+
+}
